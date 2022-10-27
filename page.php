decc6033a4295d3d5f5155640bab8e0e0cfe06fb
Munnahasan4 <73132674+freelancermunna4@users.noreply.github.com>
2022-10-28 01:27:57 +0600

    Update page.php

--- page.php
+++ page.php
@@ -28,3 +28,4 @@ $content = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM menu WHERE url='
 <?php include("common/footer.php"); ?>
 <!-- Side Navbar Links -->
 <?php if (isset($_GET['msg'])) { ?><div id="munna" data-text="<?php echo $_GET['msg']; ?>"></div><?php } ?>
+ 
